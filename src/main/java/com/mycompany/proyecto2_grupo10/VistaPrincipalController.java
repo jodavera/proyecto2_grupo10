0a12fa7b972a0a691ee1d67df0fb39f6423c3d93
Adrisiav <adrisiav@espol.edu.ec>
2021-08-16 22:39:35 -0500

    subiendo repositorio 2P

---
+++ src/main/java/com/mycompany/proyecto2_grupo10/VistaPrincipalController.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.proyecto2_grupo10;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * FXML Controller class
+ *
+ * @author Ricardo Siavichay
+ */
+public class VistaPrincipalController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void iniciarSesion(MouseEvent event) {
+    }
+
+    @FXML
+    private void simular(MouseEvent event) {
+    }
+    
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.mycompany.proyecto2_grupo10 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens com.mycompany.proyecto2_grupo10 to javafx.fxml;
     exports com.mycompany.proyecto2_grupo10;
