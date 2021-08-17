215fa40d33b94607f28fb18f10264589063d5b92
Adrisiav <adrisiav@espol.edu.ec>
2021-08-16 22:26:30 -0500

    subiendo repositorio 2P

--- src/main/java/com/mycompany/proyecto2_grupo10/App.java
+++ src/main/java/com/mycompany/proyecto2_grupo10/App.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import static javafx.application.Application.launch;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
@@ -19,27 +20,9 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-       try {
-            FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("login.fxml"));
-            
-            Parent root = fxmlLoader.load();
-            //cree el scene y fije como nodo raiz el objeto que cargo con el fxml
-            scene = new Scene(root);
-        } catch (IOException ex) {
-            //si llegamos a este punto es porque no se pudo cargar del archivo
-            //reporte.fxml el scenegraph
-            //creamos con programacion un nuevo roort y lo fijamos a la scena
-            VBox v = new VBox(new Label("No se cargo el archivo "));
-            ex.getStackTrace();
-            scene = new Scene(v);
-            System.out.println(ex);
-
-        }
-////pruebaaaaa
-        //fije la scena al primary stage
+        scene = new Scene(loadFXML("VistaPrincipal"), 700, 700);
         stage.setScene(scene);
-
-        //muestre la aplicacion
+        stage.setTitle("Urbanización");
         stage.show();
     }
 
@@ -55,5 +38,5 @@ public class App extends Application {
     public static void main(String[] args) {
         launch();
     }
-    
+
 }
